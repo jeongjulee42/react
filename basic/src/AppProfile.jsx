49bd7efae9a616afc7ca6758aea56680a5e18a40
jeongjulee42 <ljj3342@naver.com>
2022-12-14 16:08:32 +0900

    Feat: more profile

--- basic/src/AppProfile.jsx
+++ basic/src/AppProfile.jsx
@@ -5,7 +5,16 @@ function AppProfile() {
 
   return (
     <>
-      <Profile></Profile>
+      <Profile
+        image='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80'
+        name='Jamez'
+        title='Front End'
+      ></Profile>
+      <Profile
+        image='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80'
+        name='Susan'
+        title='Back End'
+      ></Profile>
     </>
   );
 }
