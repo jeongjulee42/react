0269bb34ef41e15a34d100dbcf3ae75609d1139d
jeongjulee42 <ljj3342@naver.com>
2022-12-14 15:10:14 +0900

    Feat:Profile

--- basic/src/AppProfile.jsx
+++ basic/src/AppProfile.jsx
@@ -1,10 +1,11 @@
 import './App.css';
+import Profile from './components/Profile';
 
 function AppProfile() {
-  const name = 'eee'
+
   return (
     <>
-      
+      <Profile></Profile>
     </>
   );
 }
