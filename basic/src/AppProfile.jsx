cc0ad1f7478d8048fa1fa22de48c88c71d74ceb7
jeongjulee42 <ljj3342@naver.com>
2022-12-14 16:32:41 +0900

    Feat: add avartar

--- basic/src/AppProfile.jsx
+++ basic/src/AppProfile.jsx
@@ -1,10 +1,14 @@
 import './App.css';
+import Avartar from './components/Avartar';
 import Profile from './components/Profile';
 
 function AppProfile() {
 
   return (
     <>
+    <Avartar image='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80'
+      isNew={true}/>
+        
       <Profile
         image='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80'
         name='Jamez'
