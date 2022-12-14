d0419a64d56907a9e9642ad3cc959463b83387de
jeongjulee42 <ljj3342@naver.com>
2022-12-14 16:52:47 +0900

    Feat: add btn

--- basic/src/AppProfile.jsx
+++ basic/src/AppProfile.jsx
@@ -6,6 +6,9 @@ function AppProfile() {
 
   return (
     <>
+    <button onClick={(event) => {
+      alert('Click!')
+    }}>버튼</button>
     <Avartar image='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80'
       isNew={true}/>
         
