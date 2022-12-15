edcaf8b7926ee5f92d45fead9cf30a5450995a68
jeongjulee42 <ljj3342@naver.com>
2022-12-15 18:13:04 +0900

    Feat: add count btn

---
+++ basic/src/AppCounter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Counter from './components/Counter.jsx';
+
+export default function AppCounter() {
+    return (
+        <div>
+            <Counter/>
+        </div>
+    );
+}
+
