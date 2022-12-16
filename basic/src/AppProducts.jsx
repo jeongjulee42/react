692fc398fc5de0c7634b1a775ba89dadc765db6b
jeongjulee42 <ljj3342@naver.com>
2022-12-16 10:40:15 +0900

    Feat: add Products

---
+++ basic/src/AppProducts.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import './App.css';
+import Products from './components/Products.jsx';
+
+export default function AppProducts() {
+
+    return (
+        <div >
+
+        </div>
+    );
+}
+
