fec7f42855193d856dda7f76d2db63e66a885b27
jeongjulee42 <ljj3342@naver.com>
2022-12-16 10:46:05 +0900

    Feat: add Product settings

--- basic/src/AppProducts.jsx
+++ basic/src/AppProducts.jsx
@@ -3,10 +3,11 @@ import './App.css';
 import Products from './components/Products.jsx';
 
 export default function AppProducts() {
-
+    const [showProducts, setShowProducts] = useState(true);
     return (
         <div >
-
+            {showProducts && <Products />}
+            <button onClick={() => setShowProducts((show) => !show)}>Toggle</button>
         </div>
     );
 }
--- basic/src/index.js
+++ basic/src/index.js
@@ -7,7 +7,7 @@ import AppProducts from './AppProducts';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppProducts
+    <AppProducts />
   </React.StrictMode>
 );
 
