4bddfd74453cdcd6bf6b998ac5e2870e615aadcf
jeongjulee42 <ljj3342@naver.com>
2022-12-19 14:44:24 +0900

    Feat:add position object

--- basic-xy/src/AppXY.jsx
+++ basic-xy/src/AppXY.jsx
@@ -2,16 +2,14 @@ import React, { useState } from 'react';
 import './AppXY.css';
 
 export default function AppXY() {
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+  const [position, setPosition] = useState({x:0, y:0});
   return (
     <div className='container' onPointerMove={(e) => {
-      setX(e.clientX);
-      setY(e.clientY);
+      setPosition(prev => ({x: e.clientX, y: prev.y}))
     }}>
       <div 
         className='pointer' 
-        style={{ transform: `translate(${x}px, ${y}px)`}}
+        style={{ transform: `translate(${position.x}px, ${position.y}px)`}}
       />
     </div>
   );
